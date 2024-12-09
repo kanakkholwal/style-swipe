051b7810eb8de2511580f5f5b6a3025f05468257
Kanak <kanakkholwal@gmail.com>
2024-12-09 10:25:50 +0530

    Refactor code to optimize image loading in explore page

--- web-app/app/app/explore/page.tsx
+++ web-app/app/app/explore/page.tsx
@@ -1,9 +1,10 @@
 
 
 import { Input } from "@/components/ui/input";
-import { Link, Search } from "lucide-react";
+import { Search } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 import { getProducts } from "./action";
-
 
 export default async function Page() {
     const products = await getProducts();
@@ -23,17 +24,19 @@ export default async function Page() {
             </form>
         </section>
 
-        <main className="z-10 w-full mx-auto flex gap-5 flex-wrap items-stretch justify-evenly">
+        <main className="z-10 w-full mx-auto grid grid-cols-2 @xl:grid-cols-3 @4xl:grid-cols-4 @6xl:grid-cols-5 gap-5 flex-wrap items-stretch justify-evenly mt-10">
             {products.map((product) => {
                 return <Link href={`/product/${product.id}`}
-                    className="flex flex-col items-center bg-slate-100 hover:shadow-md transition-colors duration-500 p-3 rounded-lg"
+                    className="flex flex-col items-center bg-slate-20 transition-colors duration-500 p-3 rounded-lg border border-transparent hover:border-cyan-600"
                     key={product.id}>
-                    <img src={product.imageUrls[0]} alt={product.title} className="w-72 h-72 object-cover rounded-lg" />
+                    <Image src={product.imageUrls[0]} alt={product.title} className="w-72 h-72 object-cover rounded-lg  hover:shadow-md" width={288} height={288} />
                     <h3 className="text-xl font-bold mt-3">{product.title}</h3>
-                    <p className="text-gray-600 mt-1 truncate">{product.description}</p>
+                    <p className="text-gray-600 mt-1 truncate">
+                        {product.description.slice(0, 28)}...
+                    </p>
                     <div className="flex items-center mt-2">
                         <span className="text-lg font-bold">₹{product.price}</span>
-                        {(Number(product.mrp) > 0) ? (<span className="text-red-600 line-through ml-2">₹{product.mrp}</span>):null}
+                        {(Number(product.mrp) > 0) ? (<span className="text-red-600 line-through ml-2">₹{product.mrp}</span>) : null}
                     </div>
                 </Link>
             })}
--- web-app/app/app/layout.tsx
+++ web-app/app/app/layout.tsx
@@ -23,7 +23,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <GradientBalls className="z-0" />
+        <GradientBalls className="z-[-1] fixed" />
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 border-b">
           <div className="flex items-center gap-2 px-4 w-full">
             <SidebarTrigger className="-ml-1" size="icon_sm" />
